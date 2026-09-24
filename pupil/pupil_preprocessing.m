0a888cd116c399f96ffb63da3938ef5847ad5a6b
JulieB1234 <99982434+JulieB1234@users.noreply.github.com>
2026-09-24 17:26:36 +0200

    Implement trial functions and error checks
    Added new trial functions for active, passive, and missing trials. Enhanced error checking for trial data consistency.

--- pupil/pupil_preprocessing.m
+++ pupil/pupil_preprocessing.m
@@ -1,6 +1,8 @@
 % PUPIL PREPROCESSING
 % j boyer 2026 (a urai 2016)
 
+% REQUIRES FIELDTRIP TOOLBOX
+
 %% fixed order
 % Interpolate/Clean blinks first (using the raw data)
 % Apply High-Pass filter to the continuous, "smooth" pupil trace
@@ -178,5 +180,453 @@ for icon = conditions
 
         filename_mat = cell2mat([outputdir icon '_' num2str(isubj) '_HPfilter' num2str(HPfilter) '_' num2str(twindow(1)) '_' num2str(twindow(end)) '.mat']);
         save(filename_mat, 'data','blink_prop');
+    end
+end
+
+
+%% HELPER FUNCTIONS 
+
+function [trl, event] = my_trialfun2026active(cfg)
+
+% header and events are already in the asc structures
+% Anne Urai, 2016 / fieldtrip / j Boyer 2025 for soundfmri
+% it is the trialfun called by FT function ft_definetrial.m
+
+event   = cfg.event;
+value   = {event(find(~cellfun(@isempty,strfind({event.value},'MSG')))).value};
+sample  = [event(find(~cellfun(@isempty,strfind({event.value},'MSG')))).sample];
+
+% determine the number of samples before and after the trigger
+pretrig  = -round(cfg.trialdef.pre  * cfg.fsample);
+posttrig =  round(cfg.trialdef.post * cfg.fsample);
+
+%% collect all events of interest for those that appear for each trial
+% indices
+trialstart = contains(value,'trial_start');
+auditstim = contains(value,'auditory');
+vowel = contains(value,'vowel');
+audib = contains(value,'audibility');
+snr_num = contains(value,'snr_num');
+trialend = contains(value,'end of trial');
+
+% check they all have the same number of occurence
+total_trials = numel(value(trialstart));
+
+if ~isequal(total_trials,numel(value(auditstim)))
+    error('auditstim ≠ trialstart')
+end
+
+if ~isequal(total_trials,numel(value(audib)))
+    error('audib ≠ trialstart')
+end
+
+if ~isequal(total_trials,numel(value(vowel)))
+    error('vowel ≠ trialstart')
+end
+
+if ~isequal(total_trials,numel(value(snr_num)))
+    error('snr_num ≠ trialstart')
+end
+
+if ~isequal(total_trials,numel(value(trialend)))
+    error('trialend ≠ trialstart')
+end
+
+% load behav data
+load(cfg.behavpath); % -> trials table
+
+if ~isequal(total_trials,max(trials.trial))
+    error('mismatch between behav and pupil trial nb');
+end
+
+if ~isequal(total_trials,440) && ~isequal(total_trials,450)
+    disp('Warning: not standard number of trials --> check it manually!');
+end
+
+trl = [];
+
+sample_auditstim = sample(auditstim);
+% for pre stim analyses : sample_trialstart = sample(trialstart);
+value_snrnum = value(snr_num);
+value_vowelnum = value(vowel);
+value_audib = value(audib);
+
+% loop across trials
+for itrial = 1:total_trials
+
+    % trlbegin, trlend, offset -> get sample for audit stim
+    sample = sample_auditstim(itrial); % OR sample = sample_trialstart(itrial);
+    trlbegin = sample - pretrig;
+    trlend = sample + posttrig;
+    offset = - pretrig;
+
+    % snrnum, vowelnum
+    msgsnr = value_snrnum(itrial);
+    snr_num = str2double(extractBetween(msgsnr,'snr_num = ',','));
+    msgvow = value_vowelnum(itrial);
+    vowel_num = str2double(extractBetween(msgvow,'vowel = ',','));
+    % sanity check
+    snr_num_check = trials.snr_num(itrial);
+    vowel_num_check = trials.stimulus_num(itrial);
+    if ~isequal(snr_num,snr_num_check) || ~isequal(vowel_num,vowel_num_check)
+        error('mismatch behav / pupil')
+    end
+    
+
+    % trlcnt (in block), blockcnt
+    trlcnt = trials.trial_in_block(itrial);
+    blockcnt = trials.block(itrial);
+
+    % audibility
+    msgaudib = value_audib(itrial);
+    audib = str2double(extractBetween(msgaudib,'audibility = ',','));
+    % sanity check
+    audib_check = trials.audib(itrial);
+    if ~isequal(audib,audib_check) && ~isnan(audib_check) % if nan it's fine
+        error('mismatch behav / pupil')
+    end
+    if isnan(audib_check) && ~isnan(audib)
+        error('mismatch behav / pupil -- NaN situation')
+    end
+
+    newtrl = [trlbegin trlend offset snr_num vowel_num trlcnt blockcnt audib];
+
+    trl = [trl; newtrl];
+
+end
+
+function [trl, event] = my_trialfun2026active_missing(cfg)
+
+% header and events are already in the asc structures
+% Anne Urai, 2016 / fieldtrip / j Boyer 2025 for soundfmri
+% it is the trialfun called by FT function ft_definetrial.m
+caca = false; 
+skip = false; 
+
+% blocks shitshow
+if isfield(cfg,'subj_id')
+    caca = true; 
+end
+
+event   = cfg.event;
+value   = {event(find(~cellfun(@isempty,strfind({event.value},'MSG')))).value};
+sample  = [event(find(~cellfun(@isempty,strfind({event.value},'MSG')))).sample];
+
+% determine the number of samples before and after the trigger
+pretrig  = -round(cfg.trialdef.pre  * cfg.fsample);
+posttrig =  round(cfg.trialdef.post * cfg.fsample);
+
+%% collect all events of interest for those that appear for each trial
+% indices
+trialstart = contains(value,'trial_start');
+auditstim = contains(value,'auditory');
+vowel = contains(value,'vowel');
+audib = contains(value,'audibility');
+snr_num = contains(value,'snr_num');
+trialend = contains(value,'end of trial');
+
+% check they all have the same number of occurence
+total_trials = numel(value(trialstart));
+
+% load behav data
+load(cfg.behavpath); % -> trials table
+
+trl = [];
+
+sample_auditstim = sample(auditstim);
+value_snrnum = value(snr_num);
+value_vowelnum = value(vowel);
+value_audib = value(audib);
+
+% loop across trials
+
+for itrial = 1:(total_trials - 1)
+    % trlbegin, trlend, offset -> get sample for audit stim
+    sample = sample_auditstim(itrial);
+    trlbegin = sample - pretrig;
+    trlend = sample + posttrig;
+    offset = - pretrig;
+    % snrnum, vowelnum
+    msgsnr = value_snrnum(itrial);
+    snr_num = str2double(extractBetween(msgsnr,'snr_num = ',','));
+    msgvow = value_vowelnum(itrial);
+    vowel_num = str2double(extractBetween(msgvow,'vowel = ',','));
+    % audibility
+    msgaudib = value_audib(itrial);
+    audib = str2double(extractBetween(msgaudib,'audibility = ',','));
+    % sanity check
+    if caca == 1 % blocks shitshow
+        sbj = cfg.subj_id;
+        % define ii
+        if strcmp(sbj,'28A')
+            ii = itrial + 300;
+        elseif strcmp(sbj,'12P')
+            if itrial < 201
+                ii = itrial;
+            else
+                ii = itrial + 320;
+            end
+        elseif strcmp(sbj,'13P')
+            if itrial < 241
+                ii = itrial;
+            elseif itrial > 240 && itrial < 247
+                skip = true; % extra trials to remove: 241 242 243 244 245 246
+            elseif itrial > 246
+                ii = itrial - 6;
+            end
+        elseif strcmp(sbj,'20P')
+            ii = itrial + 280;
+        elseif strcmp(sbj,'24P')
+            ii = itrial + 240;
+        elseif strcmp(sbj,'29P')
+            ii = itrial + 200;
+        elseif strcmp(sbj,'30P') || strcmp(sbj,'32P')
+            ii = itrial + 360;
+        end
+    else
+        ii = itrial;
+    end
+    if skip == false
+        snr_num_check = trials.snr_num(ii);
+        vowel_num_check = trials.stimulus_num(ii);
+        if ~isequal(snr_num,snr_num_check) || ~isequal(vowel_num,vowel_num_check)
+            error('mismatch behav / pupil')
+        end
+        audib_check = trials.audib(ii);
+        if ~isequal(audib,audib_check) && ~isnan(audib_check) % if nan it's fine
+            error('mismatch behav / pupil')
+        end
+        if isnan(audib_check) && ~isnan(audib)
+            error('mismatch behav / pupil -- NaN situation')
+        end
+        blockcnt = trials.block(ii);
+        trlcnt = itrial;
+        newtrl = [trlbegin trlend offset snr_num vowel_num trlcnt blockcnt audib];
+        trl = [trl; newtrl];
+    end
+end
+
+function [trl, event] = my_trialfun2026passive(cfg)
+
+% header and events are already in the asc structures
+% Anne Urai, 2016 / fieldtrip / j Boyer 2025 for soundfmri
+% it is the trialfun called by FT function ft_definetrial.m
+
+event   = cfg.event;
+value   = {event(find(~cellfun(@isempty,strfind({event.value},'MSG')))).value};
+sample  = [event(find(~cellfun(@isempty,strfind({event.value},'MSG')))).sample];
+
+% nb in event we have:
+% - type: 'question', 'keypress', etc.
+% - sample: value for sample timepoint of eyelink corresponding to the
+% event
+% - value: full message, also gives info about trial nb, eg 'MSG	3250633	question, trial 23 / 40 _ block 0 / 10'
+
+
+% determine the number of samples before and after the trigger
+pretrig  = -round(cfg.trialdef.pre  * cfg.fsample);
+posttrig =  round(cfg.trialdef.post * cfg.fsample);
+
+
+%% collect all events of interest for those that appear for each trial
+% indices
+trialstart = contains(value,'trial_start');
+auditstim = contains(value,'auditory');
+question = contains(value,'question');
+vowel = contains(value,'vowel');
+snr_num = contains(value,'snr_num');
+trialend = contains(value,'end of trial');
+
+% check they all have the same number of occurence
+total_trials = numel(value(trialstart));
+
+if ~isequal(total_trials,numel(value(auditstim)))
+    error('auditstim ≠ trialstart')
+end
+
+if ~isequal(total_trials,numel(value(question)))
+    error('question ≠ trialstart')
+end
+
+if ~isequal(total_trials,numel(value(vowel)))
+    error('vowel ≠ trialstart')
+end
+
+if ~isequal(total_trials,numel(value(snr_num)))
+    error('snr_num ≠ trialstart')
+end
+
+if ~isequal(total_trials,numel(value(trialend)))
+    error('trialend ≠ trialstart')
+end
+
+% load behav data
+load(cfg.behavpath); % -> trials table
+
+if ~isequal(total_trials,max(trials.trial))
+    error('mismatch between behav and pupil trial nb');
+end
+
+
+trl = [];
+
+sample_auditstim = sample(auditstim);
+value_snrnum = value(snr_num);
+value_vowelnum = value(vowel);
+
+% loop across trials
+for itrial = 1:total_trials
+    % trlbegin, trlend, offset -> get sample for audit stim
+    sample = sample_auditstim(itrial);
+    trlbegin = sample - pretrig;
+    trlend = sample + posttrig;
+    offset = - pretrig;
+    % snrnum, vowelnum
+    msgsnr = value_snrnum(itrial);
+    snr_num = str2double(extractBetween(msgsnr,'snr_num = ',','));
+    msgvow = value_vowelnum(itrial);
+    vowel_num = str2double(extractBetween(msgvow,'vowel = ',','));
+    % sanity check
+    snr_num_check = trials.snr_num(itrial);
+    vowel_num_check = trials.stimulus_num(itrial);
+    if ~isequal(snr_num,snr_num_check) || ~isequal(vowel_num,vowel_num_check)
+        error('mismatch behav / pupil')
+    end
+    % trlcnt (in block), blockcnt
+    trlcnt = trials.trial_in_block(itrial);
+    blockcnt = trials.block(itrial);
+    % question_type, HnotH
+    question_type = trials.question_num(itrial);
+    if question_type == 3 % mindwander
+        if trials.answer_num(itrial) == 1 % sound
+            HnotH = 1; % heard
+        else
+            HnotH = 0; % not heard
+        end
+    else
+        HnotH = NaN; % not probed
+    end
+    newtrl = [trlbegin trlend offset snr_num vowel_num trlcnt blockcnt question_type HnotH];
+    trl = [trl; newtrl];
+end
+
+function [trl, event] = my_trialfun2026passive_missing(cfg)
+
+% header and events are already in the asc structures
+% Anne Urai, 2016 / fieldtrip / j Boyer 2025 for soundfmri
+% it is the trialfun called by FT function ft_definetrial.m
+
+caca = false; 
+skip = false; 
+
+% blocks shitshow
+if isfield(cfg,'subj_id')
+    caca = true; 
+end
+
+event   = cfg.event;
+value   = {event(find(~cellfun(@isempty,strfind({event.value},'MSG')))).value};
+sample  = [event(find(~cellfun(@isempty,strfind({event.value},'MSG')))).sample];
+
+% nb in event we have:
+% - type: 'question', 'keypress', etc.
+% - sample: value for sample timepoint of eyelink corresponding to the
+% event
+% - value: full message, also gives info about trial nb, eg 'MSG	3250633	question, trial 23 / 40 _ block 0 / 10'
+
+
+% determine the number of samples before and after the trigger
+pretrig  = -round(cfg.trialdef.pre  * cfg.fsample);
+posttrig =  round(cfg.trialdef.post * cfg.fsample);
+
+
+%% collect all events of interest for those that appear for each trial
+% indices
+trialstart = contains(value,'trial_start');
+auditstim = contains(value,'auditory');
+question = contains(value,'question');
+vowel = contains(value,'vowel');
+snr_num = contains(value,'snr_num');
+trialend = contains(value,'end of trial');
+
+% check they all have the same number of occurence
+total_trials = numel(value(trialstart));
+
+
+% load behav data
+load(cfg.behavpath); % -> trials table
+
+trl = [];
+
+sample_auditstim = sample(auditstim);
+value_snrnum = value(snr_num);
+value_vowelnum = value(vowel);
+
+% loop across trials
+for itrial = 1:(total_trials - 1)
+    % trlbegin, trlend, offset -> get sample for audit stim
+    sample = sample_auditstim(itrial);
+    trlbegin = sample - pretrig;
+    trlend = sample + posttrig;
+    offset = - pretrig;
+    % snrnum, vowelnum
+    msgsnr = value_snrnum(itrial);
+    snr_num = str2double(extractBetween(msgsnr,'snr_num = ',','));
+    msgvow = value_vowelnum(itrial);
+    vowel_num = str2double(extractBetween(msgvow,'vowel = ',','));
+    % sanity check
+    if caca == 1 % blocks shitshow
+        sbj = cfg.subj_id;
+        % define ii
+        if strcmp(sbj,'28A')
+            ii = itrial + 300;
+        elseif strcmp(sbj,'12P')
+            if itrial < 201
+                ii = itrial;
+            else % >200
+                ii = itrial + 120;
+            end
+        elseif strcmp(sbj,'13P')
+            if itrial < 241
+                ii = itrial;
+            elseif itrial > 240 && itrial < 247
+                skip = true; % extra trials to remove: 241 242 243 244 245 246
+            elseif itrial > 246
+                ii = itrial - 6;
+            end
+        elseif strcmp(sbj,'20P')
+            ii = itrial + 280;
+        elseif strcmp(sbj,'24P')
+            ii = itrial + 240;
+        elseif strcmp(sbj,'29P')
+            ii = itrial + 200;
+        elseif strcmp(sbj,'30P') || strcmp(sbj,'32P')
+            ii = itrial + 360;
+        end
+    else
+        ii = itrial;
+    end
+    if skip == false
+    snr_num_check = trials.snr_num(ii);
+    vowel_num_check = trials.stimulus_num(ii);
+    if ~isequal(snr_num,snr_num_check) || ~isequal(vowel_num,vowel_num_check)
+        error('mismatch behav / pupil')
+    end
+    % trlcnt (in block), blockcnt
+    trlcnt = trials.trial_in_block(ii);
+    blockcnt = trials.block(ii);
+    % question_type, HnotH
+    question_type = trials.question_num(ii);
+    if question_type == 3 % mindwander
+        if trials.answer_num(ii) == 1 % sound
+            HnotH = 1; % heard
+        else
+            HnotH = 0; % not heard
+        end
+    else
+        HnotH = NaN; % not probed
+    end
+    newtrl = [trlbegin trlend offset snr_num vowel_num trlcnt blockcnt question_type HnotH];
+    trl = [trl; newtrl];
     end
 end
